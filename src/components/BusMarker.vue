c4905ffd75d37ddff71d326a412ff140fbf8a1f5
Adam Piziak <adpiziak@gmail.com>
2019-04-11 22:34:34 -0400

    added markers

---
+++ src/components/BusMarker.vue
@@ -0,0 +1,60 @@
+<template lang="pug">
+.bus-marker
+  .ref
+    .base
+    .pulse
+</template>
+
+<style lang="sass" scoped>
+$size: 30px
+$color: #555
+@mixin centered
+  position: absolute
+  top: 50%
+  left: 50%
+  transform: translate(-50%, -50%)
+.bus-marker
+  position: absolute
+  width: $size
+  height: $size
+
+  &:hover
+    cursor: default
+
+.ref
+  position: relative
+  width: $size
+  height: $size
+
+.base
+  position: relative
+  z-index: 10
+  @include centered
+  width: $size*0.3
+  height: $size*0.3
+  background: $color
+  border-radius: 50%
+
+.pulse
+  position: relative
+  width: $size
+  height: $size
+  z-index: 9
+  background: $color
+  opacity: 0.1
+  border-radius: 50%
+  animation:
+    name: pulse
+    duration: 2.5s
+    timing-function: ease-out
+    iteration-count: infinite
+
+@keyframes pulse
+  from
+    opacity: .9
+    transform: scale(.2)
+  to
+    opacity: 0
+    transform: scale(1)
+
+</style>
---
+++ src/components/StopMarker.vue
@@ -0,0 +1,70 @@
+<template lang="pug">
+.stop-marker(@click="goToStop")
+  .ref
+    .base
+      .text {{ name.substring(0, 2) }}
+</template>
+
+<script>
+import EventBus from '@/event-bus.js';
+
+export default {
+  props: {
+    id: Number,
+    name: String
+  },
+  methods: {
+    goToStop() {
+      this.$router.push(`/stops/${this.id}`)
+      EventBus.$emit('GO_TO_STOP', this.id);
+    }
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+$size: 30px
+@mixin centered
+  position: absolute
+  top: 50%
+  left: 50%
+  transform: translate(-50%, -50%)
+
+.stop-marker
+  position: absolute
+  height: $size
+  width: $size
+
+  &:hover
+    cursor: pointer
+
+
+.ref
+  position: relative
+  height: $size
+  width: $size
+
+.base
+  position: absolute
+  height: $size
+  width: $size
+  background: white
+  border-radius: 50px
+  box-shadow: 0 1px 5px rgba(#000000, 0.1)
+  border: 2px solid #e37d7d
+  transition: top .2s, width .2s
+  text-align: center
+  line-height: $size*0.94
+  font-weight: 600
+  font-size: .9rem
+  color: #666
+  overflow: hidden
+  transition: transform 0.1s
+
+  &:hover
+    box-shadow: 0 1px 5px rgba(#000000, 0.3)
+    transition: transform 0.05s
+    transform: scale(1.2)
+
+
+</style>
